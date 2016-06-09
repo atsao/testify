e2b6aa238fa8f49904deebf57275458e3626f4e4
atsao <andrew.tsao8@gmail.com>
2016-06-08 17:26:19 -0700

    e2e testing attempted

--- test/e2e/todoFunctionality.js
+++ test/e2e/todoFunctionality.js
@@ -11,3 +11,20 @@ casper.test.begin('App is setup correctly', 2, function suite(test) {
 });
 
 
+casper.test.begin('Adds an item', 1, function suite(test) {
+  // var testLength = this.items.length;
+  
+  casper.start('http://localhost:3000/', function() {
+    // test.assert(this.items.length);
+    this.fill('input', { todo: 'Test todo'}, true);
+    // test.assertExists()
+  });
+
+  casper.then(function() {
+    test.assertExists('input[data-todo-id=8]');
+  });
+
+  casper.run(function() {
+    test.done();
+  })
+});
